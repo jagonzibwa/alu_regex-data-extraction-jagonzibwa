46088f1062c88270b41c9a2e0b226d2ce0436b7c
jagonzibwa <j.agonzibwa@alustudent.com>
2025-05-20 00:19:23 +0300

    rest of functions added

--- regex_extractor.js
+++ regex_extractor.js
@@ -2,3 +2,18 @@ function isValidHTMLTag(tag) {
     const pattern = /^<\/?[a-zA-Z][a-zA-Z0-9-]\b[^>]>$/;
     return pattern.test(tag);
 }
+
+function isValidHashtag(hashtag) {
+    const pattern = /^#[a-zA-Z0-9]+$/;
+    return pattern.test(hashtag);
+}
+
+function isValidAmount(amount) {
+    const pattern = /^\$\d{1,3}(,\d{3})*\.\d{2}$/;
+    return pattern.test(amount);
+}
+
+function isValidPhoneNumber(phone) {
+    const pattern = /^(\(?\d{3}\)?)[-. ](\d{3})[-.](\d{4})$/;
+    return pattern.test(phone);
+}
